6be98b0477c78aa7e0305d58cf9e7d6aa9b558a6
Oleksandr Koreshev <a.koreshev1@gmail.com>
2023-01-05 22:08:10 +0200

    Refactor ErrorCodes Class to Enum

--- exception/src/main/java/com/epam/esm/exceptions/ErrorCodes.java
+++ exception/src/main/java/com/epam/esm/exceptions/ErrorCodes.java
@@ -1,25 +1,53 @@
 package com.epam.esm.exceptions;
 
-public final class ErrorCodes {
-    public static final String SUFFIX_RESPONSE_ENTITY_EXCEPTIONS = "99";
-    public static final String NOT_CHANGE_ROW = "42201";
-    public static final String NOT_CREATE_ROW = "42202";
-    public static final String NOT_MODIFIED_RESOURCE = "30401";
-    public static final String NOT_FOUND_TAG_RESOURCE = "40401";
-    public static final String NOT_FOUND_CERTIFICATE_RESOURCE = "40402";
-    public static final String NO_HANDLER_FOUND = "40403";
-    public static final String SQL_DUPLICATE_ENTRY = "40001";
-    public static final String SQL_NULL_ENTRY = "40002";
-    public static final String INVALID_CERTIFICATE_PROPERTY = "40003";
-    public static final String INVALID_TAG_PROPERTY = "40004";
-    public static final String INVALID_CERTIFICATE_ID_PROPERTY = "40005";
-    public static final String INVALID_TAG_ID_PROPERTY = "40006";
-    public static final String INVALID_TAG_NAME_PROPERTY = "40007";
-    public static final String NULL_RESOURCE = "40008";
-    public static final String SQL_AMBIGUOUS_COLUMN = "40009";
-    public static final String INTERNAL_SERVER_ERROR = "50001";
-    public static final String INTERNAL_SERVER_ILLEGAL_STATE = "50002";
+public enum ErrorCodes {
+    SUFFIX_RESPONSE_ENTITY_EXCEPTIONS(
+            99,
+            "Suffix for response request exception to create error custom code"),
+    NOT_CHANGE_ROW(42201, "Any row of database isn't changed by query"),
+    NOT_CREATE_ROW(42202, "Any row of database isn't created by query"),
+    NOT_FOUND_TAG_RESOURCE(40401, "Unable to find tag in database"),
+    NOT_FOUND_CERTIFICATE_RESOURCE(40402, "Unable to find gift certificate in database"),
+    NO_HANDLER_FOUND(40403, "Unable to find handler for api request"),
+    SQL_DUPLICATE_ENTRY(40001, "Attempting to create a duplicate entry in database"),
+    SQL_NULL_ENTRY(40002, "Attempting to create a null row in database"),
+    INVALID_CERTIFICATE_PROPERTY(40003, "Invalid gift certificate's property"),
+    INVALID_TAG_PROPERTY(40004, "Invalid gift tag's property"),
+    INVALID_CERTIFICATE_ID_PROPERTY(40005, "Invalid gift certificate's id property"),
+    INVALID_TAG_ID_PROPERTY(40006, "Invalid gift tad's id property"),
+    INVALID_TAG_NAME_PROPERTY(40007, "Invalid gift tag's name property"),
+    DATA_ACCESS_EXCEPTION(40009, "Unable get or put data to database"),
+    INTERNAL_SERVER_ERROR(50001, "An error or exception occurred on the server side"),
+    NULL_INSTEAD_LIST(50002, "Returns null instead of List<>"),
+    SQL_ERROR(50003, "Default code for all sql exceptions");
 
-    private ErrorCodes() {
+    private final int code;
+
+    private final String reasonPhrase;
+
+    ErrorCodes(int code, String reasonPhrase) {
+        this.code = code;
+        this.reasonPhrase = reasonPhrase;
+    }
+
+    /**
+     * Return the String value of this status code.
+     */
+    public String stringCode() {
+        return this.code + "";
+    }
+
+    /**
+     * Return the Integer value of this status code.
+     */
+    public int getCode() {
+        return this.code;
+    }
+
+    /**
+     * Return the reason phrase of this error code.
+     */
+    public String getReasonPhrase() {
+        return this.reasonPhrase;
     }
 }
